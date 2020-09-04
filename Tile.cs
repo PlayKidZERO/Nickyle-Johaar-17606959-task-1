658339e1ef5555329887315b6fa1bb5d94102901
Nickyle101 <56158397+Nickyle101@users.noreply.github.com>
2020-09-04 17:19:47 +0200

    obsticle class
    class set up

---
+++ Tile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nickyle_Johaar_17606959_task_1
+{
+    public abstract class Tile
+    {
+        protected int x;
+        protected int y;
+
+        public Tile(int xPos, int yPos)
+        {
+            this.x = xPos;
+            this.y= yPos;
+        }
+    }
+    public enum TileType
+    {
+        Hero,
+        Enemy,
+        Gold,
+        weapon,
+    }
+   
+}
