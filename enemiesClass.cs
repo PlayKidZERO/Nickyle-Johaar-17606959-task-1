600dbb788c97857f764160fcecd689902aa1240c
Nickyle101 <56158397+Nickyle101@users.noreply.github.com>
2020-09-15 02:23:35 +0200

    enemy class link
    linking enemy to player damage

--- enemiesClass.cs
+++ enemiesClass.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Nickyle_Johaar_17606959_task_1
 {
-    class enemiesClass
+    //enemy class that controlls damage 
+    abstract class enemiesClass: character
     {
+        protected Random select = new Random();
+
+        public enemiesClass(int positionx, int positiony, char character, int playerhp,int playerdamage ) : base(positionx, positiony, character)
+        {
+            PlayerMaxHP = playerhp;
+            PlayerHP = playerhp;
+            PlayerDamage = playerdamage;
+        }
+
+        public override string ToString()
+        {
+            return "by [" + PositonX + "," + PositionY + "] (" + PlayerDamage + ")";
+        }
     }
 }
