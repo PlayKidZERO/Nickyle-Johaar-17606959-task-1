657764fcbd657a88bccec2ce2f498f8d71e3c84e
Nickyle101 <56158397+Nickyle101@users.noreply.github.com>
2020-09-15 02:48:10 +0200

    creation of goblin class
    this hold the random values and moves of the player as well as the goblin enemies

---
+++ GoblinClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nickyle_Johaar_17606959_task_1
+{
+    class GoblinClass:enemiesClass
+    {
+        //Goblin class that holds and controll random values
+        public GoblinClass(int positionx, int positiony, char character = 'G', int playerDamage = 1, int playerHp = 10) : base(positionx, positiony, character, playerDamage, playerHp)
+        {
+
+        }
+        //this is the returnmove method
+        public override MovementOfCharacter ReturnMove(MovementOfCharacter movementOfPlayer = 0)
+        {
+            int randomRollSelect = select.Next(0, 5);
+
+            while (movementOfPlayer == 0)
+            {
+                if (randomRollSelect == 0)
+                {
+                    if (PlayerObserver[0] is emptyTile)
+                    {
+                        movementOfPlayer = MovementOfCharacter.Left;
+                        PositonX--;
+                    }
+                    else
+                    {
+                        randomRollSelect = select.Next(0, 5);
+                    }
+                }
+                else if (randomRollSelect == 1)
+                {
+                    if (PlayerObserver[1] is emptyTile)
+                    {
+                        movementOfPlayer = MovementOfCharacter.Up;
+                        PositionY--;
+                    }
+                    else
+                    {
+                        randomRollSelect = select.Next(0, 5);
+                    }
+                }
+                else if (randomRollSelect == 2)
+                {
+                    if (PlayerObserver[2] is emptyTile)
+                    {
+                        movementOfPlayer = MovementOfCharacter.Right;
+                        PositonX++;
+                    }
+                    else
+                    {
+                        randomRollSelect = select.Next(0, 5);
+                    }
+                }
+                else if (randomRollSelect == 3)
+                {
+                    if (PlayerObserver[3] is emptyTile)
+                    {
+                        movementOfPlayer = MovementOfCharacter.Down;
+                        PositionY++;
+                    }
+                    else
+                    {
+                        randomRollSelect = select.Next(0, 5);
+                    }
+                }
+                else
+                {
+                    movementOfPlayer = MovementOfCharacter.NoMovement;
+                }
+            }
+            return movementOfPlayer;
+        }
+    }
+}
