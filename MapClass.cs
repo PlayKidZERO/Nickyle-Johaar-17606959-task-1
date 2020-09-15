6199b4774136c74da61399d0cb171e1a981cf767
Nickyle101 <56158397+Nickyle101@users.noreply.github.com>
2020-09-15 05:04:08 +0200

    Map class
    creation of map class

---
+++ MapClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nickyle_Johaar_17606959_task_1
+{
+    class MapClass
+    {
+        private int widthOfGameMap;
+        private int heightOfGameMap;
+        private int numberOfenemies;
+
+        public int WidthOfGameMap
+        {
+            get { return widthOfGameMap; }
+            set { widthOfGameMap = value; }
+        }
+        public int HeightOfGameMap
+        {
+            get { return heightOfGameMap; }
+            set { heightOfGameMap = value; }
+        }
+        public int NumberOfEnemies
+        {
+            get { return numberOfenemies; }
+            set { numberOfenemies = value; }
+        }
+       
+    }
+}
